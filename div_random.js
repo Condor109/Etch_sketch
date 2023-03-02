0aa7b596a796df12c359becbd9f6098cfd5b8744
Condor <124301262+Condor109@users.noreply.github.com>
2023-03-02 01:34:42 -0500

    Add 'hover' effect to paint sscreen

--- div_random.js
+++ div_random.js
@@ -1,14 +1,27 @@
 const buttons=document.querySelectorAll("button");
 const inpt=document.querySelector("input[type=range]")
+const color=document.querySelector("input[type=color]")
+var c=color.value;
+var conf=false;
 const div=document.createElement('div');
 const dv=document.createElement('div');
 const main=document.querySelector(".dmain_2");
-for (let i=0; i<17; i++){
-  div.appendChild(dv.cloneNode(true));
-}
-for(let i=0;i<17;i++){
-  main.appendChild(div.cloneNode(true));
-}
+
+
+main.addEventListener('mousedown',function(e){
+  e.preventDefault();
+  conf=true;
+})
+main.addEventListener('mouseup',()=>{
+  conf=false;
+})
+
+color.addEventListener('change',function(e){
+  c=e.currentTarget.value;
+})
+
+inpt.value=16;
+generate(16);
 
 inpt.addEventListener('change',function(e){
   let v=Math.round(1+(e.currentTarget.value*63/100));
@@ -19,8 +32,7 @@ inpt.addEventListener('change',function(e){
 function clear(v){
   while (main.firstChild && div.firstChild){
     div.removeChild(div.lastChild);
-    main.removeChild(main.lastChild);
-    
+    main.removeChild(main.lastChild);  
   }
   generate(v);
 }
@@ -35,12 +47,33 @@ function generate(row_col){
     console.log(main)
     main.appendChild(div.cloneNode(true));
   }
+  const dv_2=document.querySelectorAll(".dmain_2 div div");
+  dv_2.forEach((dv_1)=>{
+    dv_1.addEventListener('mouseover',function(e){
+      if(conf==true){
+        e.currentTarget.style.backgroundColor=c
+      }
+    })
+    dv_1.addEventListener('click',function(e){
+      e.currentTarget.style.backgroundColor=c
+      
+    })
+  })
 
 }
 
 buttons.forEach((button)=>{
-  button.addEventListener('focus',function(e){
-    
+  let i=0;
+  button.addEventListener('click',function(e){
+    if(i==0){
+      e.currentTarget.style.backgroundColor="rgba(188, 177, 219, 0.425)";
+      i++
+    }
+    else{
+      e.currentTarget.style.backgroundColor="transparent"
+
+      i--;
+    }
   });
 })
 
