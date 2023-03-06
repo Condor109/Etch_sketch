532fdfb480ccceac3d4c233f306add2643f1106b
Condor <124301262+Condor109@users.noreply.github.com>
2023-03-06 16:39:10 -0500

    Completed sketch with correct mouse function

--- div_random.js
+++ div_random.js
@@ -1,37 +1,39 @@
 const buttons=document.querySelectorAll("button");
 const inpt=document.querySelector("input[type=range]")
 const color=document.querySelector("input[type=color]")
-const subd=document.querySelector("#subd_1")
-const sudb_1=document.querySelector("#subd_2")
 var c=color.value;
 var conf=false;
+var j=0;
 var i=false;
 var rain=false;
-var shadow=false;
 const div=document.createElement('div');
 const dv=document.createElement('div');
 const main=document.querySelector(".dmain_2");
 
-subd.addEventListener('click',function(e){
+subd_1.addEventListener('click',function(e){
   if(buttons[0]==e.target && buttons[0].style.backgroundColor=="aliceblue"){
     e.target.style.backgroundColor="transparent";
     rain=false;
     c=color.value;
+    i=false;
   }
   else if(buttons[1]==e.target && buttons[1].style.backgroundColor=="aliceblue"){
     e.target.style.backgroundColor="transparent";
     c=color.value;
+    i=false;
   }
   else if(buttons[0]==e.target){
     e.target.style.backgroundColor="aliceblue"
     buttons[1].style.backgroundColor="transparent";
     rain=true;
+    i=true;
 
   }
   else if(buttons[1]==e.target){
     e.target.style.backgroundColor="aliceblue"
     buttons[0].style.backgroundColor="transparent";
     rain=false;
+    i=true;
     c="transparent"
   }
   if(buttons[2]==e.target){
@@ -60,21 +62,25 @@ subd_2.addEventListener('click',function(e){
   }
 })
 
+main.addEventListener('mouseover',function(e){
+  if(rain){
+    rainbow();
+  }
+  if(conf){
+    e.target.style.backgroundColor=c;
+  }
+})
+document.addEventListener('mousedown',()=>conf=true)
+document.addEventListener('mouseup',()=>conf=false)
 
 main.addEventListener('mousedown',function(e){
-  e.preventDefault();
-  conf=true;
-})
-
-main.addEventListener('mouseleave',()=>{
-  conf=false;
-})
-
-main.addEventListener('mouseup',()=>{
-  conf=false;
+    e.preventDefault();
 })
 
 color.addEventListener('change',function(e){
+  if(i){
+    return
+  }
   c=e.currentTarget.value;
 })
 
@@ -104,7 +110,7 @@ function generate(row_col){
   for(let i=0;i<row_col;i++){
     main.appendChild(div.cloneNode(true));
   }
-  const dv_2=document.querySelectorAll(".dmain_2 div div");
+  /*const dv_2=document.querySelectorAll(".dmain_2 div div");
   dv_2.forEach((dv_1)=>{
     dv_1.addEventListener('mouseover',function(e){
       if(conf){
@@ -120,8 +126,7 @@ function generate(row_col){
       e.currentTarget.style.backgroundColor=c
       
     })
-  })
-
+  })*/
 }
 
 function rainbow(){
